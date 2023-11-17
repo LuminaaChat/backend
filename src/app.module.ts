55246f648b5b2ce03c9ac772a53767144f7f2ed3
NilsStawowyAdesso <123561547+NilsStawowyAdesso@users.noreply.github.com>
2023-11-17 20:23:55 +0100

    added user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { MessagesGateway } from './messages/messages.gateway';
 import { FcmModule } from './fcm/fcm.module';
 import { ChatModule } from './chat/chat.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-    imports: [FcmModule, ChatModule],
+    imports: [FcmModule, ChatModule, UserModule],
     controllers: [AppController],
     providers: [AppService, MessagesGateway],
 })
