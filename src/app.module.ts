998f7479dcbf991dc33402b11a7c7a2ebb45481b
NilsStawowyAdesso <123561547+NilsStawowyAdesso@users.noreply.github.com>
2023-11-17 19:48:06 +0100

    added fcm and chat feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FcmController } from './features/fcm/controller/fcm.controller';
+import { ChatController } from './features/chat/controller/chat.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, FcmController, ChatController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/features/fcm/controller/fcm.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('fcm')
+export class FcmController {}
