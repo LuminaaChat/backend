b796a16a6139648da6416a9d4f59fe688ff371a6
Dennis Hering <dennis@hering.dev>
2023-11-17 21:11:10 +0100

    Add Docker Support with MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MessagesGateway } from './messages/messages.gateway';
-import { FcmModule } from './fcm/fcm.module';
-import { ChatModule } from './chat/chat.module';
-import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import {UsersModule} from "./user/user.module";
 
 @Module({
-    imports: [FcmModule, ChatModule, UserModule, AuthModule],
+    imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(
+            `${process.env.MONGO_DB_URL}:${process.env.MONGO_DB_PORT}`,
+            {
+                dbName: process.env.MONGO_DB_DATABASE,
+                auth: {
+                    username: process.env.MONGO_DB_ROOT_USER,
+                    password: process.env.MONGO_DB_ROOT_PASSWORD,
+                },
+                autoCreate: true,
+            },
+        ),
+        UsersModule,
+        AuthModule,
+    ],
     controllers: [AppController],
-    providers: [AppService, MessagesGateway],
+    providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.setGlobalPrefix('api');
-    await app.listen(3000);
+
+    app.setGlobalPrefix('api/v1');
+
+    const config = new DocumentBuilder()
+        .setTitle('WUD.Chat')
+        .setDescription('The wud.chat API description')
+        .setVersion('1.0')
+        .addServer('http://localhost:3000')
+        .addBearerAuth()
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    fs.writeFileSync('./wud-openapi-spec.json', JSON.stringify(document));
+    SwaggerModule.setup('docs', app, document);
+
+    app.enableCors();
+
+    await app.listen(process.env.APP_PORT);
+
+    console.log(`Start App on Port ${process.env.APP_PORT}`);
 }
 bootstrap();
