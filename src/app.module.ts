36592834c2d7fd39f9f1ba9585b0813d7f6dd86d
NilsStawowyAdesso <123561547+NilsStawowyAdesso@users.noreply.github.com>
2023-11-17 19:57:53 +0100

    lint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FcmController } from './features/fcm/controller/fcm.controller';
 import { ChatController } from './features/chat/controller/chat.controller';
+import { FcmController } from './features/fcm/controller/fcm/fcm.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController, FcmController, ChatController],
-  providers: [AppService],
+    imports: [],
+    controllers: [AppController, FcmController, ChatController],
+    providers: [AppService],
 })
 export class AppModule {}
